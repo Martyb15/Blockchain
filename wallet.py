1d0a2cc3cc849c55383faf11f960fe5ae5fb58c4
Martin Barrios <60498446+Martyb15@users.noreply.github.com>
2026-01-14 21:39:35 -0800

    Fix typos in keypair and address functions

--- wallet.py
+++ wallet.py
@@ -2,10 +2,10 @@ import hashlib
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import ec
 
-def gen_keypair() -> tuble[str, str]:
+def gen_keypair() -> tuple[str, str]:
     """Return (priv_pem, pub_pem) - same format as transaction.py"""
     priv     = ec.generate_private_key(ec.SECP256K1())
-    priv_pem = priv.priavate_bytes(
+    priv_pem = priv.private_bytes(
         serialization.Encoding.PEM,
         serialization.NoEncryption(),
     ).decode()
@@ -15,9 +15,9 @@ def gen_keypair() -> tuble[str, str]:
     ).hex()
     return priv_pem, pub_hex
 
-def address_from_pub_pem(pub_pem: bytes) -> str:
-    """addr = RIPEMD160(SHA256(compressed_pubkey)) in hex."""
-    pub_bytes = bytes.formhex(pub_hex)
+def address_from_pub_pem(pub_hex: str) -> str:
+    """addr = RIPEMD160(SHA256(pubkey)) in hex."""
+    pub_bytes = bytes.fromhex(pub_hex)
     h = hashlib.sha256(pub_bytes).digest()
     ripemd = hashlib.new("ripemd160", h).hexdigest()
     return "0x" + ripemd
